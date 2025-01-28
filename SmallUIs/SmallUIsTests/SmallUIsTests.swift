99a59601a6cecb943fadd51e24ff3f67d468b57d
y-hirakaw <130817538+y-hirakaw@users.noreply.github.com>
2025-01-28 22:26:30 +0900

    プロジェクト追加

---
+++ SmallUIs/SmallUIsTests/SmallUIsTests.swift
@@ -0,0 +1,10 @@
+import Testing
+@testable import SmallUIs
+
+struct SmallUIsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
